0994fa726e0ee90c81ed7efe5ce2a6fb40be9e5f
Jack Hsu <jack.hsu@gmail.com>
2024-02-08 15:01:15 -0500

    update patches

--- scripts/patch.js
+++ scripts/patch.js
@@ -14,7 +14,6 @@ if (!content.includes('PATCHED')) {
     `
   );
   fs.writeFileSync(file, updated);
-  console.log(fs.readFileSync(file).toString());
 } else {
   console.log('already patched');
 }
@@ -23,15 +22,30 @@ file = 'node_modules/@nx/jest/src/plugins/plugin.js';
 content = fs.readFileSync(file).toString();
 if (!content.includes('PATCHED')) {
   const updated = content.replace(
-    `async (configFilePath, options, context)=>{`,
+    `async (configFilePath, options, context) => {`,
     `
     async (configFilePath, options, context)=>{
     //PATCHED
-    console.log('>>>> VITE RUNNING');
+    console.log('>>>> JEST RUNNING');
     `
   );
   fs.writeFileSync(file, updated);
-  console.log(fs.readFileSync(file).toString());
+} else {
+  console.log('already patched');
+}
+
+file = 'node_modules/@nx/playwright/src/plugins/plugin.js';
+content = fs.readFileSync(file).toString();
+if (!content.includes('PATCHED')) {
+  const updated = content.replace(
+    `async (configFilePath, options, context) => {`,
+    `
+    async (configFilePath, options, context)=>{
+    //PATCHED
+    console.log('>>>> PLAYWRIGHT RUNNING');
+    `
+  );
+  fs.writeFileSync(file, updated);
 } else {
   console.log('already patched');
 }
