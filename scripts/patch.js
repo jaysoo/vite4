62618f5c1cf028095b50bb4b3d2c03dd36045241
Jack Hsu <jack.hsu@gmail.com>
2024-02-08 14:54:35 -0500

    update patch

--- scripts/patch.js
+++ scripts/patch.js
@@ -1,15 +1,33 @@
 const fs = require('node:fs');
 
-const file = 'node_modules/@nx/vite/src/plugins/plugin.js';
-const content = fs.readFileSync(file).toString();
+let file, content;
+
+file = 'node_modules/@nx/vite/src/plugins/plugin.js';
+content = fs.readFileSync(file).toString();
 if (!content.includes('PATCHED')) {
   const updated = content.replace(
     `async (configFilePath, options, context)=>{`,
     `
     async (configFilePath, options, context)=>{
     //PATCHED
-    console.log('>>>> VITE PATCH 1');
-    return {};
+    console.log('>>>> VITE RUNNING');
+    `
+  );
+  fs.writeFileSync(file, updated);
+  console.log(fs.readFileSync(file).toString());
+} else {
+  console.log('already patched');
+}
+
+file = 'node_modules/@nx/jest/src/plugins/plugin.js';
+content = fs.readFileSync(file).toString();
+if (!content.includes('PATCHED')) {
+  const updated = content.replace(
+    `async (configFilePath, options, context)=>{`,
+    `
+    async (configFilePath, options, context)=>{
+    //PATCHED
+    console.log('>>>> VITE RUNNING');
     `
   );
   fs.writeFileSync(file, updated);
