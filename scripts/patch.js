d962cc3a2686c3f8165e1489bbe867b63d2a6988
Jack Hsu <jack.hsu@gmail.com>
2024-02-08 16:13:34 -0500

    skip loading vite config

--- scripts/patch.js
+++ scripts/patch.js
@@ -30,9 +30,10 @@ file = 'node_modules/@nx/jest/src/plugins/plugin.js';
 content = fs.readFileSync(file).toString();
 if (!content.includes('PATCHED')) {
   const updated = content.replace(
-    `async (configFilePath, options, context) => {`,
+    // `async (configFilePath, options, context) => {`,
+    `calculatedTargets[hash] = targets;`,
     `
-    async (configFilePath, options, context)=>{
+    calculatedTargets[hash] = targets;
     //PATCHED
 
     const { resolveConfig } = await import('vite');
