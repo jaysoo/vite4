b79cfa059349b5dcc00497a5234e2ae7eb995356
Jack Hsu <jack.hsu@gmail.com>
2024-02-09 08:33:30 -0500

    skip playwright loading

--- scripts/patch.js
+++ scripts/patch.js
@@ -36,13 +36,13 @@ if (!content.includes('PATCHED')) {
     calculatedTargets[hash] = targets;
     //PATCHED
 
-    //const { resolveConfig } = await import('vite');
-    //await resolveConfig({ configFile: path_1.join(context.workspaceRoot, 'apps/demo/vite.config.js'), mode: 'development' }, 'build');
+    const { resolveConfig } = await import('vite');
+    await resolveConfig({ configFile: path_1.join(context.workspaceRoot, 'apps/demo/vite.config.js'), mode: 'development' }, 'build');
 
-    const config_utils_1 = require('@nx/devkit/src/utils/config-utils');
-    delete process['__pw_initiator__'];
-    await (0, config_utils_1.loadConfigFile)(path_1.join(context.workspaceRoot, 'apps/demo-e2e/playwright.config.js'));
-    delete process['__pw_initiator__'];
+    //const config_utils_1 = require('@nx/devkit/src/utils/config-utils');
+    //delete process['__pw_initiator__'];
+    //await (0, config_utils_1.loadConfigFile)(path_1.join(context.workspaceRoot, 'apps/demo-e2e/playwright.config.js'));
+    //delete process['__pw_initiator__'];
     console.log('>>>> JEST RUNNING');
     `
   );
