b01e4d005b98e839a35e203bb7913a21afc34587
Jack Hsu <jack.hsu@gmail.com>
2024-02-09 09:12:09 -0500

    trace

--- apps/demo/vite.config.js
+++ apps/demo/vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
+console.trace('>>>>> demo vite.config.js');
+
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/demo',
